9bdb52de5b02082e76273e7cdd28f3468c671592
ka1rakan <kairaskaraca@gmail.com>
2024-11-06 10:39:09 +1100

    feat: Create basic save and fetch database mechanism using localStorage

--- Frontend/src/types.ts
+++ Frontend/src/types.ts
@@ -7,3 +7,12 @@ interface Task {
 interface Database {
   tasks: Task[];
 }
+
+export function save(data: Database) {
+  localStorage.setItem("data", JSON.stringify(data));
+}
+
+export function fetch(): Database {
+  const data = localStorage.getItem("data") as string;
+  return JSON.parse(data);
+}
