f20bb5e229a644742f83508bee055037a79f11e2
ka1rakan <kairaskaraca@gmail.com>
2024-11-08 18:21:48 +1100

    chore: Git rm deleted files

--- Frontend/src/pages/Countdown.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Countdown() {
-  return (
-    <div>Countdown</div>
-  )
-}
--- Frontend/src/pages/Planning.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Planning() {
-  return (
-    <div>Planning</div>
-  )
-}
